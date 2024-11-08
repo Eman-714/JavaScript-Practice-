02f912d6867979d409f41eccda4a4275d59ed3b0
Eman <eman786shahid@gmail.com>
2024-11-08 19:32:38 +0000

    Practice String

---
+++ JavaScript_Basic/string.js
@@ -0,0 +1,36 @@
+console.log("Hello");
+let name="My Name is Eman Shahid"
+//let a="is"
+//console.log(name.charAt(4));
+//console.log(name.lastIndexOf(a));
+//let b=name.concat(" ",a)
+//console.log(b);
+//let splitString=name.split(" ")
+//console.log(splitString);
+//console.log(splitString[0]);
+// slice Function 
+
+//let anotherString=name.slice(4)
+//console.log(anotherString);
+
+// Replace 
+let url="https://emanshaid.com"
+console.log("Before replacing:",url);
+
+url=url.replace('eman','Muhammad')
+console.log("Afer replacing:",url);
+console.log(url.includes("shaid"))
+let urlnew=url.split(".")
+console.log(urlnew);
+const icons = "Hello";
+for(let i=0;i<icons.length;i++){
+  console.log(icons[i].codePointAt()) 
+}
+//
+
+
+
+
+
+
+
