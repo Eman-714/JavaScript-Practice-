8b8a9b1dde2bdb938334df0a22ade689f09d6c4a
Eman <eman786shahid@gmail.com>
2024-11-09 15:09:25 +0000

    date and its function

---
+++ JavaScript_Basic/Date.js
@@ -0,0 +1,11 @@
+//Date and it's function
+let myDate= new Date()// declare
+console.log(myDate);
+// use function to output readable
+console.log("Use function to date readable");
+console.log(myDate.toString());
+console.log(myDate.toDateString())
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
